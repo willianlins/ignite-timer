f8214b18b152b7cf149d4f8d1e21a8de099696cb
Willian Lins Araújo <willian.araujo@anchieta.br>
2024-04-10 16:31:10 -0300

    routes e eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
-    <>
-     <h1>testse</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
---
+++ src/pages/History.tsx
@@ -0,0 +1,3 @@
+export function History() {
+  return <h1>Hitory</h1>
+}
